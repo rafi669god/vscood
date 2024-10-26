aa9aa8a697b18ffa3399380d88d5d7220239ef50
rafi669god <rafiqulislam6201@gmail.com>
2024-10-26 15:36:37 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let text; 
+document.getElementById("btn1").onclick = function (){
+  text =  document.getElementById("in1").value  
+    document.getElementById("p1").textContent = `${text}`
+        
+}
